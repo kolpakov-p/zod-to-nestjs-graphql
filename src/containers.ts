9c8725ecf98f9feb3fe24ddf30dd585108b4f7c6
Pavel Kolpakov <kolpakov.p@gmail.com>
2023-12-06 20:21:30 +0400

    Add support of unions and literals

--- src/containers.ts
+++ src/containers.ts
@@ -1,7 +1,8 @@
-import { AnyZodObject } from "zod";
+import { AnyZodObject, ZodUnion } from "zod";
 import { ClassType } from "@nestjs/graphql/dist/enums/class-type.enum";
 
 export const typeContainers = {
   [ClassType.INPUT]: new WeakMap<AnyZodObject, any>(),
   [ClassType.OBJECT]: new WeakMap<AnyZodObject, any>(),
+  Union: new WeakMap<ZodUnion<any>, any>(),
 };
